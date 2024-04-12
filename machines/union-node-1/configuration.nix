4f0300de307b3597687c475694840dad8b401875
Jurriaan Pruijs <jurriaan@pruijs.nl>
2024-04-12 16:59:54 +0200

    cleanup

--- machines/union-node-1/configuration.nix
+++ machines/union-node-1/configuration.nix
@@ -7,18 +7,9 @@
     ];
 
   boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "union-node-1";
   networking.domain = "";
   users.users.root.openssh.authorizedKeys.keys = keys;
-  # users.users.union = {
-  #   isSystemUser = true;
-  #   group = "union";
-  #   description = "Union daemon user";
-  #   extraGroups = [ "docker" ];
-  # };
-  # users.groups.union = {};
   services.openssh.enable = true;
-  # system.stateVersion = "23.11";
 }
