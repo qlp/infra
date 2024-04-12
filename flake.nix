1671da8f623f7f7a8409997f6f68fc1392bcbbc5
Jurriaan Pruijs <jurriaan@pruijs.nl>
2024-04-12 15:57:06 +0200

    Configure union-visor using nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    union.url = "git+ssh://git@github.com/unionlabs/union";
   };
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, union, ... }:
     let
       keys = [
         ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB4rUA+CKIC1RK6NVxMaPkYIABhs5zL2Hwdxu4HSrpOH jurriaan@pruijs.nl'' # jurriaan
@@ -10,16 +11,46 @@
     in
     {
       nixosConfigurations = {
-        union-node-1 = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            ./machines/union-node-1/configuration.nix
-            ./modules/docker.nix
-            {
-              _module.args = { inherit keys; };
-            }
-          ];
-        };
+        union-node-1 = 
+          let
+            system = "aarch64-linux";
+            pkgs = import nixpkgs { inherit system; };
+          in
+          nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              ./machines/union-node-1/configuration.nix
+              ./modules/docker.nix
+              union.nixosModules.unionvisor
+              {
+                system.stateVersion = "23.11";
+                # Base configuration for openstack-based VPSs
+                imports = [ "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix" ];
+
+                # Allow other validators to reach you
+                networking.firewall.allowedTCPPorts = [ 80 443 26656 26657 ];
+
+                # Unionvisor module configuration
+                services.unionvisor = {
+                  enable = true;
+                  moniker = "qlp-1";
+                };
+
+                # OPTIONAL: Some useful inspection tools for when you SSH into your validator
+                environment.systemPackages = with pkgs; [
+                  bat
+                  bottom
+                  helix
+                  jq
+                  neofetch
+                  tree
+                ];
+              }
+              {
+                _module.args = { inherit keys; };
+              }
+            ];
+          };
       };
       formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
--- machines/union-node-1/configuration.nix
+++ machines/union-node-1/configuration.nix
@@ -1,24 +1,24 @@
 { config, lib, pkgs, keys, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+  # imports =
+  #   [
+  #     ./hardware-configuration.nix
+  #   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "union-node-1";
   networking.domain = "";
   users.users.root.openssh.authorizedKeys.keys = keys;
-  users.users.union = {
-    isSystemUser = true;
-    group = "union";
-    description = "Union daemon user";
-    extraGroups = [ "docker" ];
-  };
-  users.groups.union = {};
+  # users.users.union = {
+  #   isSystemUser = true;
+  #   group = "union";
+  #   description = "Union daemon user";
+  #   extraGroups = [ "docker" ];
+  # };
+  # users.groups.union = {};
   services.openssh.enable = true;
-  system.stateVersion = "23.11";
+  # system.stateVersion = "23.11";
 }
