f6f63cd52fbaf5a8dcb0b62bc537527702d9c518
jurriaan <jurriaan@pruijs.nl>
2024-11-25 20:28:59 +0100

    use shared authorized keys

--- machines/testnet-9/configuration.nix
+++ machines/testnet-9/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ keys, ... }: {
   imports = [
     ./hardware-configuration.nix
     ./networking.nix # generated at runtime by nixos-infect
@@ -10,8 +10,6 @@
   networking.hostName = "testnet-9";
   networking.domain = "";
   services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
-    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB4rUA+CKIC1RK6NVxMaPkYIABhs5zL2Hwdxu4HSrpOH'' 
-  ];
+  users.users.root.openssh.authorizedKeys.keys = keys;
   system.stateVersion = "24.04";
 }
